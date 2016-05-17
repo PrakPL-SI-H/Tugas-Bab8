f4a896ae9bbca7d6caca1f3bd98b418a60efdb4f
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-17 22:08:39 +0700

    edit class main class bab8

---
+++ src/tugas/bab8/Bab8.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bab8;
+
+/**
+ *
+ * @author Prasetyo
+ */
+public class Bab8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
