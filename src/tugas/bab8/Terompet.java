72b4bfdfe1adecb748636f936c5d693ff1ddb579
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-17 22:12:12 +0700

    edit anak class alat music tiup"terompet"

---
+++ src/tugas/bab8/Terompet.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bab8;
+
+/**
+ *
+ * @author Prasetyo
+ */
+public class Terompet extends AlatMusicTiup implements InterfaceAlatMusic{
+    public void caraMemainkan(){
+        System.out.println("Di Tiup");
+    }
+    @Override
+    public void NamaAlatMusic(){
+    
+    }
+    @Override
+    public void berbunyi(){}
+}
+
