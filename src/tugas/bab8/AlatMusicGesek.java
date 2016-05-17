42bdac89288f3374e642b52853ce92c3d46c533e
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-17 22:07:24 +0700

    edit class abstract induk class AlatMusicGesek

---
+++ src/tugas/bab8/AlatMusicGesek.java
@@ -0,0 +1,10 @@
+package bab8;
+public abstract class AlatMusicGesek implements InterfaceAlatMusic{
+    public void caraMemainkan(){
+        System.out.println("Di Gesek");
+    }
+    @Override
+    public void NamaAlatMusic(){}
+    @Override
+    public void berbunyi(){}
+}
