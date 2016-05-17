9c8064c4d686eadfccf4c799572e413ce30ebb48
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-17 23:58:07 +0700

    edit abstrack class induk AlatMusicPetik

--- src/tugas/bab8/AlatMusicPetik.java
+++ src/tugas/bab8/AlatMusicPetik.java
@@ -1,5 +1,9 @@
 package bab8;
 public abstract class AlatMusicPetik implements InterfaceAlatMusic{
+    public void AlatMusicPetik(){
+    
+    }
+    @Override
     public void caraMemainkan(){
         System.out.println("Di Petik");
     }
