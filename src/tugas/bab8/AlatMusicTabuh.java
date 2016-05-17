f3df527759ba322e2945a63cddbd18c9206c9649
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-17 22:07:50 +0700

    edit class abstract induk class AlatMusicTabuh

---
+++ src/tugas/bab8/AlatMusicTabuh.java
@@ -0,0 +1,10 @@
+package bab8;
+public abstract class AlatMusicTabuh implements InterfaceAlatMusic{
+    public void caraMemainkan(){
+        System.out.println("Di Tabuh");
+    }
+    @Override
+    public void NamaAlatMusic(){}
+    @Override
+    public void berbunyi(){}
+}
