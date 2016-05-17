9251fc1434135c84cea37915e180c7004b72c4fe
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-17 22:08:10 +0700

    edit class abstract induk class AlatMusicTiup

---
+++ src/tugas/bab8/AlatMusicTiup.java
@@ -0,0 +1,10 @@
+package bab8;
+public abstract class AlatMusicTiup implements InterfaceAlatMusic{
+    public void caraMemainkan(){
+        System.out.println("Di Tiup");
+    }
+    @Override
+    public void NamaAlatMusic(){}
+    @Override
+    public void berbunyi(){}
+}
