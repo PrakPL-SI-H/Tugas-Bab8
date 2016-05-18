66083b20445eb5e8075df3898826d1fd6c1adcc8
wenigustirahayu <wenigr97@gmail.com>
2016-05-18 12:55:16 +0700

    Membuat class Suling extends Tiup

---
+++ src/tugas/bab8/Suling.java
@@ -0,0 +1,19 @@
+
+package mainalatmusik;
+
+public class Suling extends Tiup {
+
+    public Suling(String nama) {
+        super(nama);
+    }
+
+    @Override
+    public String nada() {
+        return "C";
+    }
+
+    @Override
+    public String bahan() {
+        return "Bambu";
+    }
+}
