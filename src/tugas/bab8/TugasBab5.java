fe15ce76d377637f6afa07af089c0875f9c3a143
rodhirf <rodhirobb@gmail.com>
2016-05-18 19:19:53 +0700

    membuat main class

--- src/tugas/bab8/TugasBab5.java
+++ src/tugas/bab8/TugasBab5.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package tugas.bab5;
-
-/**
- *
- * @author irwin deriyan
- */
-public class TugasBab5 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public class Musik {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.println("Selamat Datang di I-Musik");
+System.out.println("-----------------------------");
+System.out.println("Pilih alat musik anda : ");
+System.out.println("1. Gitar ");
+System.out.println("2. Flute ");
+System.out.println("Pilih : ");
+int alat = input.nextInt();
+if (alat == 1){
+System.out.println("Anda memilih Gitar");
+}else{
+System.out.println("Anda memilih Flute");
+}
+System.out.println("-----------------------------");
+System.out.println("Pilih Nada Dasar Do- : ");
+System.out.println("1. C");
+System.out.println("2. D");
+System.out.println("3. E");
+System.out.println("4. F");
+System.out.println("5. G");
+System.out.println("6. A");
+System.out.println("7. B");
+System.out.println("Pilih : ");
+int nada = input.nextInt();
+System.out.println("=============================");
+if (alat == 1){
+Petik p = new Gitar("Kayu","Petik",nada);
+p.display();
+p.nadaDasar();
+}if (alat == 2){
+Tiup t = new Flute("Logam","Tiup",nada);
+t.display();
+t.nadaDasar();
+}  
+}
 }
