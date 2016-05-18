886492fd58f775094f4ded10634fa8ae59e2fc01
indahdwichyntia <indahdwicr@gmail.com>
2016-05-18 20:37:29 +0700

    class biola

---
+++ src/Biola.java
@@ -0,0 +1,18 @@
+package Interface.AlatMusik;
+public class Biola extends AlatMusikGesek {
+    public Biola(String namaAlatMusik) {
+        super(namaAlatMusik);
+    }
+     @Override
+    public String caraMain() {
+        return super.caraMain();
+    }   
+    @Override
+    public String nadaDasar() {
+        return "G";
+    }
+    @Override
+    public String bahanPembuatan() {
+        return "Kayu";
+    }   
+}
