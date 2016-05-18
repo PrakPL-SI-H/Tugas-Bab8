6434e0f0d37e0c58f3df0787441b920dcf7490d1
indahdwichyntia <indahdwicr@gmail.com>
2016-05-18 20:35:18 +0700

    class bass

---
+++ src/Bass.java
@@ -0,0 +1,18 @@
+package Interface.AlatMusik;
+public class Bass extends AlatMusikPetik {
+    public Bass(String namaAlatMusik) {
+        super(namaAlatMusik);
+    }
+    @Override
+    public String caraMain() {
+        return super.caraMain();
+    }   
+    @Override
+    public String nadaDasar() {
+        return "C";
+    }
+    @Override
+    public String bahanPembuatan() {
+        return "Kayu";
+    }
+}
