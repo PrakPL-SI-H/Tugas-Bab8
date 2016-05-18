1340fbde985f751ab376d36f74b69f810ddcd41f
istighfarinba <bafarcorp@gmail.com>
2016-05-18 18:14:02 +0700

    pembuatan class untuk men-set Nada

---
+++ src/SetNada.java
@@ -0,0 +1,17 @@
+public class SetNada  { //parent class untuk SetNada(pengganti nada default)
+    private String Alat,Do,Re,Mi,Fa,So,La,Si,DoTinggi;
+    
+  public SetNada(String Alat,String Do,String Re,String Mi,String Fa,String So,String La,String Si,String DoTinggi) {
+      this.Alat=Alat;
+      this.Do=Do;
+      this.Re=Re;
+      this.Mi=Mi;
+      this.Fa=Fa;
+      this.So=So;
+      this.La=La;
+      this.Si=Si;
+      this.DoTinggi=DoTinggi;
+  }
+  public SetNada(){}
+ 
+}
