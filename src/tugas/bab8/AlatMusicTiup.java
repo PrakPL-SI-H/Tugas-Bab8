fad671904a7ee87836b3b7d3aee6fbdb7eb054b6
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-18 12:41:17 +0700

    edit final abstract class AlatMusicTiup

--- src/tugas/bab8/AlatMusicTiup.java
+++ src/tugas/bab8/AlatMusicTiup.java
@@ -1,11 +1,10 @@
 package bab8;
 public abstract class AlatMusicTiup implements InterfaceAlatMusic{
     public void AlatMusicTiup(){
-    
     }
     @Override
     public void caraMemainkan(){
-        System.out.println("Di Tiup");
+        System.out.println("Memainkan Dengan Cara Di Tiup");
     }
     @Override
     public void NamaAlatMusic(){}
