a14ed6649aa119a5433ae954e49eb2253b13a958
istighfarinba <bafarcorp@gmail.com>
2016-05-18 18:29:36 +0700

    pembuatan class Seruling(pemanggilan attribut class main+overide methodnya)

---
+++ src/Seruling.java
@@ -0,0 +1,23 @@
+
+public class Seruling extends AlatMusikTiup implements Musik {
+//child class dari class AlatMusikTiup dan implemen musik
+    public Seruling(String Nama, String Bahan, String CaraPakai) {
+        super(Nama, Bahan, CaraPakai);
+    }
+
+    @Override
+    public void showNama() {
+        System.out.println("Alat musik bernama      :" + super.getNama());
+    }
+
+    @Override
+    public void showBahan() {
+        System.out.println("Terbuat dari            :" + super.getBahan());
+    }
+
+    @Override
+    public void CaraPakai() {
+        System.out.println("Dimainkan dengan cara   :" + super.getCaraPakai());
+    }
+
+}
