a628e41616679af31ea7c7a6fe32e436f0349080
dyahas <arifiani.dyah@gmail.com>
2016-05-18 18:59:39 +0700

    alatmusik

---
+++ src/tugas/bab8/AlatMusik.java
@@ -0,0 +1,15 @@
+package mainmusik;
+
+public interface AlatMusik {
+
+    public void namaAlat();
+
+    public void caraMemainkan();
+
+    public void nadaDasar();
+
+    public void bahanAlat();
+
+    public void penemu();
+
+}
