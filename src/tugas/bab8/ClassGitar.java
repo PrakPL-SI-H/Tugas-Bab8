3163acb5f19a7e963aa77149b814f894e5417b4b
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-18 21:23:17 +0700

    final class gitar

--- src/tugas/bab8/ClassGitar.java
+++ src/tugas/bab8/ClassGitar.java
@@ -5,10 +5,52 @@
  */
 package Praktikum_Laporan8;
 
+import java.util.Scanner;
+
 /**
  *
  * @author TEGAR
  */
 public class ClassGitar extends Abstrak_Petik {
-    
-}
+   String nama = "Gitar";
+    int nada;
+    Scanner ini= new Scanner(System.in);
+     @Override
+     public void caraMemainkan(){
+
+        System.out.println("Memainkan Dengan Cara Dipetik");
+     }
+     @Override
+     public void NamaAlat(){
+
+        System.out.println("Nama Alat Musik Ini Adalah = " +nama);
+     }
+    @Override
+    public void bunyi(){
+        System.out.println("=========={{{{{++++++++++}}}}}==========");
+        System.out.println("Mainkan Alat Musik Dengan Memasukkan Angka dengan ketentuan: ");
+        System.out.println("1 = DO, 2 = RE, 3 = MI, 4 = FA dan 5 = SOL");
+        System.out.println(" untuk berhenti silahkan memasukkan angka 0");
+        System.out.println("=========={{{{{++++++++++}}}}}==========");
+        System.out.print("Masukkan nada (DO,RE,MI,FA,SOL) = ");
+        do{
+        nada = ini.nextInt();
+        switch(nada){
+            case 1:
+                System.out.print(" DO ");break;
+            case 2:
+                System.out.print(" RE ");break;
+            case 3 :
+                System.out.print(" MI ");break;
+            case 4 :
+                System.out.print(" FA ");break;
+            case 5 : 
+                System.out.print(" SOL ");break;
+            default :
+               System.out.println("{{ Nada yang anda masukkan salah atau tidak ditemukan}}");
+            }
+        }while(nada != 0);
+            System.out.println("Anda telah menghentikan alat musik");
+        }
+ }
+
