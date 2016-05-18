8ecf5e2f7af93f2c113886eb168f9dd395234786
arifardhan <ariframadhan902@yahoo.com>
2016-05-18 21:03:55 +0700

    selesai membuat class Sexophone

--- src/Sexophone.java
+++ src/Sexophone.java
@@ -1,5 +1,33 @@
 package Praktikum8;
 
 public class Sexophone extends Tiup {
-    
+
+    private String merk, warna;
+    private int harga = 750000;
+
+    public Sexophone (String m, String w) {
+        merk = m;
+        warna = w;
+    }
+
+    public void tampil() {
+        System.out.println("Merk  : " + merk);
+        System.out.println("Warna : " + warna);
+    }
+
+    public int getHarga() {
+        System.out.println("Harga : " + harga);
+        return harga;
+    }
+
+    @Override
+    public void nada() {
+        System.out.println("Nada : do = b");
+    }
+
+    @Override
+    public void bahan() {
+        System.out.println("Bahan : Besi");
+    }
+
 }
