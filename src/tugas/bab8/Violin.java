4d3a659eaf734f9dbf313a5b847cf3a13ea8b02a
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-05-18 18:51:47 +0700

    add class Violin

---
+++ src/tugas/bab8/Violin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prakproglan.bab8;
+
+/**
+ *
+ * @author Aldo
+ */
+public class Violin extends AlatMusikGesek {
+    
+    public String alatMusik(){
+        return "Violin";
+    }
+    public String nadaDasar(){
+        return "C D E F G A B dan minor mayornya";
+    }
+    public String bahan(){
+        return "Kayu";
+    }
+    
+}
