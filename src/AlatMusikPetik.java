e9a275b3b6d9485d6a6b7fb54c114ba65e5a5a57
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 20:41:00 +0700

    2

--- src/AlatMusikPetik.java
+++ src/AlatMusikPetik.java
@@ -8,3 +8,13 @@ public class AlatMusikPetik implements AlatMusik {
     public void Mainkan() {
         System.out.println(getNama() + " dimainkan dengan cara dipetik");
     }
+    
+    @Override
+    public void SetelNada() {
+        System.out.println("Nada dasar yang digunakan pada " + getNama() + " adalah C = Do");
+    }
+
+    @Override
+    public void BahanAlat() {
+        System.out.println(getNama() + " terbuat dari bahan kayu");
+    }
