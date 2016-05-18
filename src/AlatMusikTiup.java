dd8282d419a716178329dcf0aabfc2e2b8a08f39
istighfarinba <bafarcorp@gmail.com>
2016-05-18 18:39:20 +0700

    deklarasi variabel baru+pembuatan konstruktor lagi -,-

--- src/AlatMusikTiup.java
+++ src/AlatMusikTiup.java
@@ -1,5 +1,15 @@
 
 public class AlatMusikTiup {//pembuatan parent class untuk penentu cara main
 
+    private String Nama;
+    private String Bahan;
+    private String CaraPakai;
+
+    public AlatMusikTiup(String Nama, String Bahan, String CaraPakai) {
+        this.Nama = Nama;
+        this.Bahan = Bahan;
+        this.CaraPakai = CaraPakai;
+    }
+
 
 }
