a89d1113f5d5deba63b04714aa92c6b50ee8113c
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-05-18 18:47:49 +0700

    add interface AlatMusik

---
+++ src/tugas/bab8/AlatMusik.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prakproglan.bab8;
+
+/**
+ *
+ * @author Aldo
+ */
+public interface AlatMusik {
+    
+    public String alatMusik();
+    public String caraMain();
+    public String nadaDasar();
+    public String bahan();
+    
+}
