d6ee7d3aaee57b6cfb12b19e52fe31bc388046c3
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 20:53:59 +0700

    bass

--- src/Bass.java
+++ src/Bass.java
@@ -1,1 +1,13 @@
+package alatmusik;
 
+public class Bass extends AlatMusikPetik {
+
+    public Bass(String nama) {
+        this.nama = nama;
+    }
+
+    @Override
+    public void SetelNada() {
+        System.out.println("Nada dasar yang digunakan pada " + getNama() + " adalah G = Do");
+    }
+}
