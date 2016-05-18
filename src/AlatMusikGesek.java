ca02469547679d09c751d2cef747c6ccde343d54
istighfarinba <bafarcorp@gmail.com>
2016-05-18 18:34:42 +0700

    deklarasi variabel+pembuatan konstruktor+method

--- src/AlatMusikGesek.java
+++ src/AlatMusikGesek.java
@@ -1,4 +1,15 @@
+
 public class AlatMusikGesek { //pembuatan parent class untuk penentu cara main
+
+    private String Nama;
+    private String Bahan;
+    private String CaraPakai;
+
+    public AlatMusikGesek(String Nama, String Bahan, String CaraPakai) {
+        this.Nama = Nama;
+        this.Bahan = Bahan;
+        this.CaraPakai = CaraPakai;
+    }
 
 
 }
