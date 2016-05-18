4699d3b4b8c905cc641d840901a37a3513c40f66
Lasmatriani <lasmatriani@gmail.com>
2016-05-18 21:26:38 +0700

    1

---
+++ src/tugas/bab8/AlatMusik.java
@@ -0,0 +1,14 @@
+package Bab8;
+
+public interface AlatMusik {
+
+    public String tampilkanKunci();
+
+    public String tampilkanNamaAlat();
+
+    public String tampilkanCaraBermain();
+
+    public String tampilkanBahan();
+
+    public String getDescription();
+}
