2649e41bad6af17fb27ae2358448537a9cdc81d2
istighfarinba <bafarcorp@gmail.com>
2016-05-18 18:27:34 +0700

    pembuatan class Sasando(pemanggilan attribut class parent AlatMusikPetik+pembuatan overide method)

---
+++ src/Sasando.java
@@ -0,0 +1,24 @@
+
+public class Sasando extends AlatMusikPetik implements Musik {
+//child class dari class AlatMusikPetik dan implemen musik
+    public Sasando(String Nama, String Bahan, String CaraPakai) {
+        super(Nama, Bahan, CaraPakai);
+    }
+
+    @Override
+    public void showNama() {
+        System.out.println("Alat musik bernama      :" + super.getNama());
+    }
+
+    @Override
+    public void showBahan() {
+        System.out.println("Terbuat dari            :" + super.getBahan());
+    }
+
+    @Override
+    public void CaraPakai() {
+        System.out.println("Dimainkan dengan cara   :" + super.getCaraPakai());
+    }
+
+    
+}
