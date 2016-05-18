90574b8a0d05c666d7333e80e3b418dede7ad94b
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 20:47:49 +0700

    mainkan

--- src/AlatMusikTabuh.java
+++ src/AlatMusikTabuh.java
@@ -1,1 +1,10 @@
+package alatmusik;
 
+public class AlatMusikTabuh implements AlatMusik {
+
+    String nama;
+
+    @Override
+    public void Mainkan() {
+        System.out.println(getNama() + " dimainkan dengan cara ditabuh");
+    }
