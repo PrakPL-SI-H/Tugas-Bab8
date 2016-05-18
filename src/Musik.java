ec287507f17ec43bf29c277a68009e92aa2d65f7
istighfarinba <bafarcorp@gmail.com>
2016-05-18 18:05:35 +0700

    membuat class interface(Musik.java)dan deklarasi method

---
+++ src/Musik.java
@@ -0,0 +1,7 @@
+public interface Musik { //pembuatan class interface untuk implementasi
+    public void showNama();
+    public void showBahan();
+    public void NadaDasar();
+    public void CaraPakai();
+    
+}
