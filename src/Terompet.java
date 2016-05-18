8c42b6ced4614b8c551a64e49fe194a954ba683c
indahdwichyntia <indahdwicr@gmail.com>
2016-05-18 20:35:51 +0700

    class terompet

---
+++ src/Terompet.java
@@ -0,0 +1,18 @@
+package Interface.AlatMusik;
+public class Terompet extends AlatMusikTiup{
+    public Terompet(String namaAlatMusik) {
+        super(namaAlatMusik);
+    }
+    @Override
+    public String caraMain() {
+        return super.caraMain();
+    }
+    @Override
+    public String nadaDasar() {
+        return "C";
+    }
+    @Override
+    public String bahanPembuatan() {
+        return "Logam";
+    }   
+}
