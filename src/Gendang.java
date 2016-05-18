2597dea45a7e1054a08a931864017973c55e3d07
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 21:02:58 +0700

    ndangge

--- src/Gendang.java
+++ src/Gendang.java
@@ -1,1 +1,15 @@
+package alatmusik;
+
+public class Gendang extends AlatMusikTabuh {
+
+    public Gendang(String nama) {
+        this.nama = nama;
+    }
+
+    @Override
+    public void SetelNada() {
+        System.out.println("Tidak ada nada dasar pada alat musik " + getNama() + ", hanya megeluarkan suara Ndang Ndang Ndang");
+    }
+
+}
 
