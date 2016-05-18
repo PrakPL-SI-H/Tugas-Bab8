df6e7d14157184da7aca66c0f2fcf489383e4e22
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 21:08:57 +0700

    do

--- src/MainMusik.java
+++ src/MainMusik.java
@@ -13,3 +13,14 @@ public class MainMusik {
         Terompet t = new Terompet("Terompet");
         Gendang ge = new Gendang("Gendang");
         Bedug be = new Bedug("Bedug");
+        do {
+            System.out.println("============================");
+            System.out.println("MENU MACAM-MACAM ALAT MUSIK");
+            System.out.println("============================");
+            System.out.println("1. Alat Musik Petik");
+            System.out.println("2. Alat Musik Tiup");
+            System.out.println("3. Alat Musik Tabuh");
+            System.out.println("4. Exit");
+            System.out.print("Masukkan pilihan Anda : ");
+            pilihan = in.nextInt();
+            System.out.println(" ");
