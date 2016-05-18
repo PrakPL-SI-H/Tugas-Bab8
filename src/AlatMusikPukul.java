2ba8d792522003a35cf31a6e93aa67f27039d2dc
istighfarinba <bafarcorp@gmail.com>
2016-05-18 18:38:12 +0700

    deklarasi variabel baru+pembuatan konstruktor class

--- src/AlatMusikPukul.java
+++ src/AlatMusikPukul.java
@@ -1,5 +1,16 @@
 
 public class AlatMusikPukul {//pembuatan parent class untuk penentu cara main
 
+    private String Nama;
+    private String Bahan;
+    private String CaraPakai;
+
+    public AlatMusikPukul(String Nama, String Bahan, String CaraPakai) {
+        this.Nama = Nama;
+        this.Bahan = Bahan;
+        this.CaraPakai = CaraPakai;
+    }
+
+   
 
 }
