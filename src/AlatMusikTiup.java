d08e1d8e11a246f3cb2c46acce49b6b9474228b6
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 20:45:45 +0700

    tiup

--- src/AlatMusikTiup.java
+++ src/AlatMusikTiup.java
@@ -1,1 +1,10 @@
+package alatmusik;
 
+public class AlatMusikTiup implements AlatMusik {
+
+    String nama;
+
+    @Override
+    public void Mainkan() {
+        System.out.println(getNama() + " dimainkan dengan cara ditiup");
+    }
