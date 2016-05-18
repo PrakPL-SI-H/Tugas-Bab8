17b8df4be5b22250d7aecb45e898eb932b938fd9
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-18 12:40:46 +0700

    edit final abstract class AlatMusicPetik

--- src/tugas/bab8/AlatMusicPetik.java
+++ src/tugas/bab8/AlatMusicPetik.java
@@ -1,11 +1,10 @@
 package bab8;
 public abstract class AlatMusicPetik implements InterfaceAlatMusic{
     public void AlatMusicPetik(){
-    
     }
     @Override
     public void caraMemainkan(){
-        System.out.println("Di Petik");
+        System.out.println("Memainkan Dengan Cara Di Petik");
     }
     @Override
     public void NamaAlatMusic(){}
