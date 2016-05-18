16808e6964d2ebbaa81c54db2dc0d3ea6a5a220e
arifardhan <ariframadhan902@yahoo.com>
2016-05-18 20:51:41 +0700

    selesai membuat class Tiup

--- src/Tiup.java
+++ src/Tiup.java
@@ -1,5 +1,14 @@
 package Praktikum8;
 
-public class Tiup {
+public class Tiup implements AlatMusik {
 
+    @Override
+    public void nada() {
+        System.out.println("");
+    }
+
+    @Override
+    public void bahan() {
+        System.out.println("");
+    }
 }
