5abc9e3133cbe7813318387374cc53f3286b0928
bavianadin <bavianadin@gmail.com>
2016-05-18 22:34:03 +0700

    buat alat musik

---
+++ src/AlatMusikTiup.java
@@ -0,0 +1,7 @@
+package PackageInterface;
+public abstract class AlatMusikTiup implements IAlatMusik {
+    public String nada;
+    public void caraMain(){
+        System.out.println("Dimainkan dengan Ditiup");
+    }
+}
