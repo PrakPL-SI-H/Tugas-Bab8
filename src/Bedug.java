063315711a8e867be093ea9a1ad4513421aeef9d
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 20:55:38 +0700

    ye

--- src/Bedug.java
+++ src/Bedug.java
@@ -1,1 +1,14 @@
+package alatmusik;
+
+public class Bedug extends AlatMusikTabuh {
+
+    public Bedug(String nama) {
+        this.nama = nama;
+    }
+
+    public void SetelNada() {
+        System.out.println("Tidak ada nada dasar pada alat musik " + getNama() + ", hanya megeluarkan suara Dug Dug Dug");
+    }
+
+}
 
