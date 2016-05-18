b6384bc09020fd0345e9624e9138019ce04bb508
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 21:08:14 +0700

    objek

--- src/MainMusik.java
+++ src/MainMusik.java
@@ -1,1 +1,15 @@
+package alatmusik;
 
+import java.util.Scanner;
+
+public class MainMusik {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int pilihan, pilih;
+        Bass b = new Bass("Bass");
+        Gitar g = new Gitar("Gitar");
+        Harmonika h = new Harmonika("Harmonika");
+        Terompet t = new Terompet("Terompet");
+        Gendang ge = new Gendang("Gendang");
+        Bedug be = new Bedug("Bedug");
