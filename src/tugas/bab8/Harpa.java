d07ef56ed19bc2570ec9af88c07e8ba4fe115b4b
wenigustirahayu <wenigr97@gmail.com>
2016-05-18 19:03:58 +0700

    Menambahkan class Harpa extends Petik

---
+++ src/tugas/bab8/Harpa.java
@@ -0,0 +1,18 @@
+
+package mainalatmusik;
+public class Harpa extends Petik {
+
+    public Harpa (String nama) {
+        super(nama);
+    }
+
+    @Override
+    public String nada() {
+        return "E Minor";
+    }
+
+    @Override
+    public String bahan() {
+        return "Kayu Cendana";
+    }
+}
