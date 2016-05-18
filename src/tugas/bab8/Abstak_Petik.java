0f34c3b7033068ef0aabdc863e147e0d0726521e
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-18 20:55:04 +0700

    membuat override dan implement untuk class Abtract_Petik

--- src/tugas/bab8/Abstak_Petik.java
+++ src/tugas/bab8/Abstak_Petik.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Praktikum_Laporan8;
 
 /**
  *
  * @author TEGAR
  */
-public abstract class Abstak_Petik {
+public abstract class Abstak_Petik implements InterFaceAlatMusik{
+     public void Abstak_Petik (){
     
+     }
+     @Override
+     public void caraMemainkan(){
+        
+        System.out.println("Memainkan Dengan Cara Di Petik");
+     }
+     @Override
+     public void NamaAlat(){}
+     @Override
+     public void bunyi(){}
 }
