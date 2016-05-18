52d9707fc235ab0ab9d94914c628e8c6fd577c3f
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 21:06:45 +0700

    trompet

--- src/Terompet.java
+++ src/Terompet.java
@@ -1,1 +1,13 @@
+package alatmusik;
+
+public class Terompet extends AlatMusikTiup {
+
+    public Terompet(String nama) {
+        this.nama = nama;
+    }
+
+    public void SetelNada() {
+        System.out.println("Nada dasar yang digunakan pada " + getNama() + " adalah D = Do ");
+    }
+}
 
