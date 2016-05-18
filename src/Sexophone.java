fd4879eba9a08b008fc304d386dfcf011d082283
indahdwichyntia <indahdwicr@gmail.com>
2016-05-18 20:36:32 +0700

    class sexophone

---
+++ src/Sexophone.java
@@ -0,0 +1,18 @@
+package Interface.AlatMusik;
+public class Sexophone extends AlatMusikTiup {
+    public Sexophone(String namaAlatMusik) {
+        super(namaAlatMusik);
+    }
+    @Override
+    public String caraMain() {
+        return super.caraMain();
+    }
+    @Override
+    public String nadaDasar() {
+        return "B";
+    }
+    @Override
+    public String bahanPembuatan() {
+        return "Logam";
+    }
+}
