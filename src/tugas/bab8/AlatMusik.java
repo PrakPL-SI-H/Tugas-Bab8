84f5fad2de57091fbedd653d9caf19df17b0f366
FelixYosa <felix_yosa@ymail.com>
2016-05-18 20:50:48 +0700

    edit

---
+++ src/tugas/bab8/AlatMusik.java
@@ -0,0 +1,12 @@
+//Nama : Felix Yosa H. S.
+//NIM : 155150400111129
+//Kelas : SI-H
+
+package mainalatmusik;
+
+public interface AlatMusik {
+
+    public void nada();
+
+    public void bahan();
+}
