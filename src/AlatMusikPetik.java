a03f70a4244906d4a1ce5b46ae7ee33bcaf47a1e
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 20:40:36 +0700

    1

--- src/AlatMusikPetik.java
+++ src/AlatMusikPetik.java
@@ -1,1 +1,10 @@
+package alatmusik;
 
+public class AlatMusikPetik implements AlatMusik {
+
+    String nama;
+
+    @Override
+    public void Mainkan() {
+        System.out.println(getNama() + " dimainkan dengan cara dipetik");
+    }
