a86cde6068653ba568c91addd3e1b9e1fe9a4ac9
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-18 20:57:07 +0700

    ganti nama file

---
+++ src/tugas/bab8/Abstrak_Petik.java
@@ -0,0 +1,21 @@
+
+package Praktikum_Laporan8;
+
+/**
+ *
+ * @author TEGAR
+ */
+public abstract class Abstrak_Petik implements InterFaceAlatMusik{
+     public void Abstrak_Petik (){
+    
+     }
+     @Override
+     public void caraMemainkan(){
+        
+        System.out.println("Memainkan Dengan Cara Di Petik");
+     }
+     @Override
+     public void NamaAlat(){}
+     @Override
+     public void bunyi(){}
+}
