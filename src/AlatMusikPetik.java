9b5a950991f6b9a8305d00cc54b57427f24dd614
istighfarinba <bafarcorp@gmail.com>
2016-05-18 18:36:55 +0700

    deklarasi variabel+buat konstruktor

--- src/AlatMusikPetik.java
+++ src/AlatMusikPetik.java
@@ -1,5 +1,16 @@
 
 public class AlatMusikPetik {//pembuatan parent class untuk penentu cara main
 
+    private String Nama;
+    private String Bahan;
+    private String CaraPakai;
+
+    public AlatMusikPetik(String Nama, String Bahan, String CaraPakai) {
+        this.Nama = Nama;
+        this.Bahan = Bahan;
+        this.CaraPakai = CaraPakai;
+    }
+
+   
 
 }
