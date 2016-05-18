da810aca73dcd358e73d387656ff16bcabe04c9e
Muhammad Iqbal <iqbal0804@gmail.com>
2016-05-18 07:11:36 +0700

    sekalian dah tambahin java interface Musiknya nanggung

---
+++ src/Musik.java
@@ -0,0 +1,12 @@
+package PL_Laporan_8_interface;
+
+public interface Musik {
+
+    public void tampilkanNama();
+
+    public void CaraBermain();
+
+    public void tampilkanBahan();
+
+    public void NadaDasar();
+}
