ee1e9170a407b840ef619c33ac970208abbc4820
istighfarinba <bafarcorp@gmail.com>
2016-05-18 18:41:59 +0700

    deklarasi variabel baru..

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-      
+        Scanner input = new Scanner(System.in);
+        int pilihan;
+        String ulang;
+        String Alat,doo,re,mi,fa,so,la,si,dooo;
+        
+    }
     
-}}
+}
