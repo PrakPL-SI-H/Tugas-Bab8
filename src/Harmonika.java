1dcc59d382a69866c2a7ff3d471899215e259283
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 21:05:15 +0700

    monika

--- src/Harmonika.java
+++ src/Harmonika.java
@@ -1,1 +1,13 @@
+package alatmusik;
+
+public class Harmonika extends AlatMusikTiup {
+
+    public Harmonika(String nama) {
+        this.nama = nama;
+    }
+
+    public void SetelNada() {
+        System.out.println("Nada dasar yang digunakan pada " + getNama() + " adalah C = Do ");
+    }
+}
 
