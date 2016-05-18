e5fc360c6e70988a92041ad1d257f91908aab7e9
arifardhan <ariframadhan902@yahoo.com>
2016-05-18 20:59:01 +0700

    selesai membuat class Gitar

--- src/Gitar.java
+++ src/Gitar.java
@@ -2,4 +2,32 @@ package Praktikum8;
 
 public class Gitar extends Petik {
 
+    private String merk, warna;
+    private int harga = 850000;
+
+    public Gitar(String m, String w) {
+        merk = m;
+        warna = w;
+    }
+
+    public void tampil() {
+        System.out.println("Merk  : " + merk);
+        System.out.println("Warna : " + warna);
+    }
+
+    public int getHarga() {
+        System.out.println("Harga : " + harga);
+        return harga;
+    }
+
+    @Override
+    public void nada() {
+        System.out.println("Nada : do = a");
+    }
+
+    @Override
+    public void bahan() {
+        System.out.println("Bahan : Kayu");
+    }
+
 }
