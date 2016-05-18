179e66f2d095a63af547666bda3f26fe6f6a634a
prasetyomargaretwibisono23 <tiyo16@gmail.com>
2016-05-18 12:41:03 +0700

    edit final abstract class AlatMusicGesek

--- src/tugas/bab8/AlatMusicTekan.java
+++ src/tugas/bab8/AlatMusicTekan.java
@@ -1,10 +1,10 @@
 package bab8;
 public abstract class AlatMusicTekan implements InterfaceAlatMusic{
     public void AlatMusicTekan(){
-    
     }
+    @Override
     public void caraMemainkan(){
-        System.out.println("Di Gesek");
+        System.out.println("Memainkan Dengan Cara Di Gesek");
     }
     @Override
     public void NamaAlatMusic(){}
