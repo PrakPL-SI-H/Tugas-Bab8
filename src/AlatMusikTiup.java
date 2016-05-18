572d1fced8eac70e9d2a26f2972eb64aa88c73a9
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-18 20:46:15 +0700

    tiup2

--- src/AlatMusikTiup.java
+++ src/AlatMusikTiup.java
@@ -8,3 +8,19 @@ public class AlatMusikTiup implements AlatMusik {
     public void Mainkan() {
         System.out.println(getNama() + " dimainkan dengan cara ditiup");
     }
+    
+    @Override
+    public void SetelNada() {
+        System.out.println("Nada dasar yang digunakan pada " + getNama() + " adalah G = Do ");
+    }
+
+    @Override
+    public void BahanAlat() {
+        System.out.println(getNama() + " terbuat dari bahan logam");
+    }
+
+    @Override
+    public String getNama() {
+        return nama;
+    }
+}
