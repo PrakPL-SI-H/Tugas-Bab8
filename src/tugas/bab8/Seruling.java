a3c0634719d2bece4290ef9a9f649c8f3ac4d780
Aldo Maulana Hidayat <aldzmau@gmail.com>
2016-05-18 18:51:04 +0700

    add class Seruling

---
+++ src/tugas/bab8/Seruling.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prakproglan.bab8;
+
+/**
+ *
+ * @author Aldo
+ */
+public class Seruling extends AlatMusikTiup {
+    
+    public String alatMusik(){
+        return "Seruling";
+    }
+    public String nadaDasar(){
+        return "1(Do) 2(Re) 3(Mi) 4(Fa) 5(Sol) 6(La) 7(Si)";
+    }
+    public String bahan(){
+        return "Bambu";
+    }
+    
+}
