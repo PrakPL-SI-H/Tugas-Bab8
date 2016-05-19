48f4ace56922f89de701b206441a85c54827206c
Firdha Sabiilarrasyad <Firdha Sabiilarrasyad>
2016-05-19 18:08:32 +0700

    class abstract Tabuh

---
+++ src/Tabuh.java
@@ -0,0 +1,10 @@
+
+package bab8;
+
+public abstract class Tabuh implements AlatMusik {
+    private String caraMain = "Dipukul";
+    public String getCaraMain() {
+        return caraMain;
+    }
+}
+
