007224771fa96ef01c84a566a9225fa8a7c3fd70
Firdha Sabiilarrasyad <Firdha Sabiilarrasyad>
2016-05-19 18:09:36 +0700

    class interface AlatMusik

---
+++ src/AlatMusik.java
@@ -0,0 +1,12 @@
+
+package bab8;
+
+
+    public interface AlatMusik {
+    public void namaAlat();
+    public void caraMain();
+    public void nadaDasar(String s);
+    public void bahanAlat();
+}
+
+
