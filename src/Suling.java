e0d0d47beed7c239e95dc878eb8d2559d22224a0
MochammadAK <pagarberkarat@gmail.com>
2016-05-20 18:24:01 +0700

    7

---
+++ src/Suling.java
@@ -0,0 +1,24 @@
+package bab8;
+public class Suling extends MusikTiup implements AlatMusik {
+    public Suling(String namaAlat) {
+        super(namaAlat);
+    }
+    @Override
+    public String NadaDasar() {
+         System.out.println("Pilihan Nada Dasar : ");
+        String Nada = input.nextLine();
+        if (Nada.equalsIgnoreCase("C")){
+            System.out.println("Do, Re, Mi, Fa, So, La, Si, Do");
+            System.out.println("Kunci G");
+ }
+        if (Nada.equalsIgnoreCase("A")){
+            System.out.println("La, Si, Do, Re, Mi, Fa, So, La");
+            System.out.println("Kunci F");
+        }
+         return Nada;
+    }
+    @Override
+    public String BahanBuatan() {
+        return "Bambu";
+    }
+}
