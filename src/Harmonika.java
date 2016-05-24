f7b931f30fced03d4930507d02c75fe8cbc24878
ambarsari <ambardwi010@yahoo.com>
2016-05-24 10:48:24 +0700

    g

---
+++ src/Harmonika.java
@@ -0,0 +1,23 @@
+public class Harmonika extends MusikTiup implements AlatMusik{
+    public Harmonika(String namaAlatMusik) {
+        super(namaAlatMusik);
+    }
+    @Override
+    public String NadaDasar() {
+      System.out.println("Pilihan Nada Dasar : ");
+        String Nada = input.nextLine();
+        if (Nada.equalsIgnoreCase("C")){
+            System.out.println("Di mulai dari : Do, Re, Mi, Fa, So, La, Si, Do");
+            System.out.println("Kunci G");
+ }
+        if (Nada.equalsIgnoreCase("A")){
+                        System.out.println("Di mulai dari : La, Si, Do, Re, Mi, Fa, So, La");
+ System.out.println("Kunci F");
+        }
+         return Nada;
+    }
+    @Override
+    public String BahanBuatan() {
+        return "Alumunium";
+    }
+}
